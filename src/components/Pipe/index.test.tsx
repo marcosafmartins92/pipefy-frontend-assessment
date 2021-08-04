b274594a3308d9986e13521cfa9c5ab8096ecd2f
Marcos Martins <marcos.martins@woozaventures.com.br>
2021-08-04 11:52:28 -0300

    fix: pipe test

--- src/components/Pipe/index.test.tsx
+++ src/components/Pipe/index.test.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
-import { render, screen, within } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Pipe from '.';
+import { MockedProvider } from '@apollo/client/testing';
 
 describe('Pipe Element', () => {
   const pipeExample = {name: 'José', id: 1, icon: 'icon', image: 'image'};
 
-  render(<Pipe {...pipeExample} />);
+  beforeEach(() => {
+    render(
+      <MockedProvider mocks={[]} addTypename={false}>
+        <Pipe {...pipeExample} />
+      </MockedProvider>
+    );
+  })
   
   test.each`
-    field          |   value
-    ${'name'}      |   ${'José'}
-    ${'id'}        |   ${1}
-    ${'icon'}      |   ${'icon'}
-    ${'image'}     |   ${'image'}
+    field              |   value
+    ${'name'}          |   ${'José'}
+    ${'cards-count'}   |   ${'0 cards'}
   `('should parse $value to $expected', ({ field, value }) => {
-    const { getByText } = within(screen.getByTestId(field))
-    expect(getByText(value)).toBeInTheDocument()
+    const element = screen.getByTestId(field)
+    expect(element).toHaveTextContent(value)
   })
-})
+  
+
+  test('should set icon value to class name', () => {
+    const iconElement = screen.getByTestId('icon');
+    expect(iconElement.className).toBe('icon');
+  });
+
+});
+
